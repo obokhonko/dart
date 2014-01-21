89ba247f2391aaf6e331e52d7a79b4238dd96c96
obokhonko <obokhonko@twinfield.com>
2014-01-21 18:14:54 +0200

    back-motion test

--- script.js
+++ script.js
@@ -15,4 +15,29 @@ $(function() {
             }
         }
     });
+    /*
+    var earth = $('.earth-layer'),
+        galaxy = $('.galaxy-layer'),
+        sky = $('.sky-layer'),
+        stars = $('.stars-layer'),
+        window = $(window),
+        height = window.height(),
+        pos = 1;
+
+    var backgroundShift = function() {
+        var scale;
+        console.log(height);
+        sky.css({
+            opacity: ((height * 2) - pos) / (height * 2) - 0.2
+        });
+        scale = Math.max(0, ((height * 0.5) - pos) / (height * 0.5));
+        earth.css({'transform': 'translate3d(0,' + ((pos * 1.1) + height / 2) + 'px, 0)  scale3d(' + scale + ',' + scale + ',1 )'});
+        galaxy.css({'transform': 'translate3d(0, ' + (pos * 1.05) + 'px, 0)'});
+        stars.css({'transform': 'translate3d(0, ' + (pos * 1.02) + 'px, 0)'});
+    };
+
+    document.addEventListener('scroll', function() {
+        backgroundShift();
+    });
+    */
 });
